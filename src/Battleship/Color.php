44d735eb921ba931eb749b12b070aecc9495bfc1
Paweł Balicki <pawel@kilab.dev>
2024-12-04 11:52:11 +0100

    Update ANSI color codes for better terminal compatibility

--- src/Battleship/Color.php
+++ src/Battleship/Color.php
@@ -5,11 +5,11 @@ namespace Battleship;
 class Color
 {
     const DEFAULT_GREY = "\e[39m";
-    const MAGENTA = "\e[95m";
-    const CADET_BLUE = "\e[34m";
+    const MAGENTA = "\e[35m";
+    const CADET_BLUE = "\e[36m";
     const RED = "\e[31m";
     const YELLOW = "\e[93m";
-    const ORANGE = "\e[38;5;208m";
+    const ORANGE = "\e[33m";
     const CHARTREUSE = "\e[32m";
-    const BLUE = "\e[36m";
+    const BLUE = "\e[34m";
 }
